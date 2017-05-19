f0d64f775c4b8c3a03b0e7a006b38dd9cf4570e2
U-dolzzo-PC\dolzzo <dolzzo@mutecsoft.com>
2017-05-19 16:52:21 +0900

    Refactor makeTidy()
    Reset current index to first when modification occurred.
    Don't have to call isTidy()

--- src/qualificationRound/ProblemB.java
+++ src/qualificationRound/ProblemB.java
@@ -30,26 +30,8 @@ public class ProblemB extends Problem {
             numbers = m.toCharArray();
         }
 
-        boolean isTidy() {
-            for (int i = numbers.length-1;i>0;i--) {
-                if (numbers[i-1] > numbers[i]) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
         String solve() {
-            while(!isTidy()) {
-                makeTidy();
-            }
 
-            String result = String.valueOf(numbers);
-            return String.valueOf(result.replaceFirst("^0+(?!$)", ""));
-        }
-
-        void makeTidy() {
             for (int i=1;i<numbers.length;i++) {
                 if (numbers[i-1] > numbers[i]) {
                     numbers[i-1]--;
@@ -57,8 +39,14 @@ public class ProblemB extends Problem {
                     for (int j=i;j<numbers.length;j++) {
                         numbers[j] = '9';
                     }
+
+                    i = 0;
                 }
             }
+
+            String result = String.valueOf(numbers);
+            return String.valueOf(result.replaceFirst("^0+(?!$)", ""));
         }
+
     }
 }
