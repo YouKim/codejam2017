458b4f844948e146281d6f6ee2eac8378dbd7ae7
You Kim <you.kim72@gmail.com>
2017-05-25 02:44:39 +0900

    [WIP] ???

--- src/round1A/ProblemC.java
+++ src/round1A/ProblemC.java
@@ -15,7 +15,7 @@ public class ProblemC extends Problem {
         Game game = new Game(in);
         //game.print();
         String result = game.solve();
-
+        System.out.printf("Case #%d: %s\n", testNumber, result);
         out.printf("Case #%d: %s\n", testNumber, result);
     }
 
@@ -27,8 +27,6 @@ public class ProblemC extends Problem {
         final int mBuff;
         final int mDebuff;
         HashMap<State, Integer> map;
-
-        int minimumTurn = TURN_IMPOSSIBLE;
 
         static final int TURN_MAX = 100;
         static final int TURN_IMPOSSIBLE = TURN_MAX + 5;
@@ -47,7 +45,6 @@ public class ProblemC extends Problem {
         String solve() {
             int T = simulate(mHPMax, mAT, kHP, kAT, "");
 
-            System.out.println(" T:" + T);
             if (T<=TURN_MAX) {
                 return String.valueOf(T);
             } else {
@@ -57,83 +54,70 @@ public class ProblemC extends Problem {
 
         public int simulate(int hp, int att, int knightHP, int knightATT, String commands) {
 
-            if (commands.length() > minimumTurn) {
-                return TURN_IMPOSSIBLE;
+            //System.out.println("simulate:" + hp + "," + att + "," + knightHP + "," + knightATT + "," + commands);
+
+            State state = new State(hp, att, knightHP, knightATT);
+
+            Integer key = map.get(state);
+            if (key != null) {
+                int result = key;
+
+                //System.out.println("contain:" + result);
+                return result;
             }
+
+            map.put(state, TURN_IMPOSSIBLE);
 
             if (hp <= 0) {
                 return TURN_IMPOSSIBLE;
             }
 
-            //System.out.println("simulate:" + hp + "," + att + "," + knightHP + "," + knightATT + "," + turn);
-
-            State state = new State(hp, att, knightHP, knightATT);
-
-            if (map.containsKey(state)) {
-                return map.get(state);
-            }
-
             if (knightHP - att <=0) {
                 map.put(state, 1);
-                System.out.println(commands + 'A' + ":1." + " ~ " + commands.length());
-
-                if (commands.length() + 1 < minimumTurn) {
-                    minimumTurn = commands.length() + 1;
-                }
-
                 return 1;
             }
 
-            map.put(state, TURN_IMPOSSIBLE);
 
             int minimum = TURN_IMPOSSIBLE;
             int visited;
 
-            String history = commands;
+            int debuffed = knightATT - mDebuff;
+            if (debuffed < 0) {
+                debuffed = 0;
+            }
 
-
-            if (hp <= knightATT) {
+            if (hp - debuffed <= 0) {
+                //cure
                 visited = simulate(mHPMax - knightATT, att, knightHP, knightATT, commands + 'C') + 1;
                 if (visited < minimum) {
                     minimum = visited;
-                    history = commands + 'C';
                 }
             } else {
+
+                // debuff
+                if (mDebuff > 0 && knightATT > 0 && !commands.contains("B") && !commands.contains("A") ) {
+                    visited = simulate(hp - debuffed, att, knightHP, debuffed, commands + 'D') + 1;
+                    if (visited < minimum) {
+                        minimum = visited;
+                    }
+                }
+
+                //buff
+                if (mBuff > 0 && !commands.contains("A")) {
+                    visited = simulate(hp - knightATT, att+mBuff, knightHP, knightATT, commands + 'B') + 1;
+                    if (visited < minimum) {
+                        minimum = visited;
+                    }
+                }
 
                 //attack
                 visited = simulate(hp - knightATT, att, knightHP - att, knightATT, commands + 'A') + 1;
                 if (visited < minimum) {
                     minimum = visited;
-                    history = commands + 'A';
-                }
-
-                //buff
-                if (mBuff > 0) {
-                    visited = simulate(hp - knightATT, att+mBuff, knightHP, knightATT, commands + 'B') + 1;
-                    if (visited < minimum) {
-                        minimum = visited;
-                        history = commands + 'B';
-                    }
                 }
 
 
-                //debuff
-                if (mDebuff > 0 && knightATT > 0) {
-                    int debuffed = knightATT - mDebuff;
-                    if (debuffed < 0) {
-                        debuffed = 0;
-                    }
 
-                    visited = simulate(hp - debuffed, att, knightHP, debuffed, commands + 'C') + 1;
-                    if (visited < minimum) {
-                        minimum = visited;
-                        history = commands + 'D';
-                    }
-                }
-            }
-
-            if (minimum <= TURN_MAX) {
-                System.out.println(history + ":" + minimum);
             }
 
             map.put(state, minimum);
@@ -147,7 +131,7 @@ public class ProblemC extends Problem {
                 states = new int[values.length];
 
                 int i=0;
-                for(int val:values){
+                for (int val:values){
                     states[i++] = val;
                 }
             }
