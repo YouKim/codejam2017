aacdc9e3b6850003b57584dd44bdd80a6fbd1035
U-dolzzo-PC\dolzzo <dolzzo@mutecsoft.com>
2017-05-25 18:31:25 +0900

    [WIP] Change algorithm

--- src/round1A/ProblemC.java
+++ src/round1A/ProblemC.java
@@ -1,7 +1,6 @@
 package round1A;
 
 import java.io.PrintWriter;
-import java.util.HashMap;
 
 public class ProblemC extends Problem {
 
@@ -13,37 +12,165 @@ public class ProblemC extends Problem {
     @Override
     protected void solveTest(int testNumber, InputReader in, PrintWriter out) {
         Game game = new Game(in);
-        //game.print();
         String result = game.solve();
+
         System.out.printf("Case #%d: %s\n", testNumber, result);
         out.printf("Case #%d: %s\n", testNumber, result);
     }
 
     static class Game {
-        final int mHPMax;
-        final int mAT;
-        final int kHP;
-        final int kAT;
-        final int mBuff;
-        final int mDebuff;
-        HashMap<State, Integer> map;
+        final int DH;
+        final int DA;
+        final int KH;
+        final int KA;
+        final int BF;
+        final int DB;
+
+        int mOptA;
+        int mOptB;
+        int mOptC;
+        int mOptD;
 
         static final int TURN_MAX = 100;
         static final int TURN_IMPOSSIBLE = TURN_MAX + 5;
 
         Game(InputReader in) {
-            mHPMax = in.nextInt();
-            mAT = in.nextInt();
-            kHP = in.nextInt();
-            kAT = in.nextInt();
-            mBuff = in.nextInt();
-            mDebuff = in.nextInt();
+            DH = in.nextInt();
+            DA = in.nextInt();
+            KH = in.nextInt();
+            KA = in.nextInt();
+            BF = in.nextInt();
+            DB = in.nextInt();
 
-            map = new HashMap<>();
+            findOptimalAB();
+            findOptimalCD();
         }
 
+        void findOptimalAB() {
+            int minA = TURN_IMPOSSIBLE;
+            int minB = TURN_IMPOSSIBLE;
+            int minAB = TURN_IMPOSSIBLE;
+
+            for (int B=0;B<minAB;B++) {
+                int A = attackCount(KH, DA + BF * B);
+                if (A + B < minAB) {
+                    minAB = A + B;
+                    minA = A;
+                    minB = B;
+                }
+
+                if (BF <= 0) {
+                    break;
+                }
+            }
+
+            mOptA = minA;
+            mOptB = minB;
+
+            System.out.println("findOptimalAB  A:" + minA + " B:" + minB + " BF:" + BF);
+        }
+
+        void findOptimalCD() {
+
+            int minCD = 100 - (mOptA + mOptB) + 1;
+            int minC = TURN_IMPOSSIBLE;
+            int minD = TURN_IMPOSSIBLE;
+
+            int maxTurn = TURN_MAX;
+
+            for (int debuff_limit=0;debuff_limit<minCD;debuff_limit++) {
+
+                int A = 0, B = 0, C = 0, D = 0;
+
+                int dh = DH;
+                int da = DA;
+                int kh = KH;
+                int ka = KA;
+
+                StringBuffer commands = new StringBuffer();
+
+                for (int T=1;T<=maxTurn;T++) {
+
+                    int debuffed_ka = ka;
+                    if (D < debuff_limit) {
+                        debuffed_ka = ka - DB;
+                        if (debuffed_ka < 0) {
+                            debuffed_ka = 0;
+                        }
+                    }
+
+                    if (kh - da <= 0) {
+                        A++;
+                        kh -= da;
+                        commands.append('A');
+                    } else if (dh - debuffed_ka <= 0) {
+                        //cure
+                        C++;
+                        dh = DH;
+                        commands.append('C');
+                    } else {
+                        if (D < debuff_limit && ka > 0 && DB > 0) {
+                            D++;
+                            ka = debuffed_ka;
+                            commands.append('D');
+                        } else if (B < mOptB) {
+                            B++;
+                            da += BF;
+                            commands.append('B');
+                        } else {
+                            A++;
+                            kh -= da;
+                            commands.append('A');
+                        }
+                    }
+
+                    if (kh <= 0) {
+                        System.out.println("[WIN] T:" + T + " A:" + A + " B:" + B + " C:" + C + " D:" + D + " cmds:" + commands);
+
+                        if (C + D < minCD) {
+                            minCD = C + D;
+                            minC = C;
+                            minD = D;
+                        }
+                        break;
+                    } else {
+                        // Knight's turn
+                        dh -= ka;
+
+                        if (dh <= 0) {
+                            System.out.println("[LOST] T:" + T + " cmds:" + commands);
+                            break;
+                        }
+                    }
+                }
+
+                if (DB <= 0) {
+                    break;
+                }
+            }
+
+            mOptC = minC;
+            mOptD = minD;
+            System.out.println("findOptimalCD  C:" + minC + " D:" +  minD);
+        }
+
+        int attackCount(int hp, int attack) {
+
+            /*
+            int i=0;
+            for (i=0;hp>0;i++) {
+                hp -= attack;
+            }
+
+            return i;
+            */
+
+            return hp/attack + (hp%attack>0?1:0);
+        }
+
+
         String solve() {
-            int T = simulate(mHPMax, mAT, kHP, kAT, "");
+            int T = mOptA + mOptB + mOptC + mOptD;
 
             if (T<=TURN_MAX) {
                 return String.valueOf(T);
@@ -52,122 +179,5 @@ public class ProblemC extends Problem {
             }
         }
 
-        public int simulate(int hp, int att, int knightHP, int knightATT, String commands) {
-
-            //System.out.println("simulate:" + hp + "," + att + "," + knightHP + "," + knightATT + "," + commands);
-
-            State state = new State(hp, att, knightHP, knightATT);
-
-            Integer key = map.get(state);
-            if (key != null) {
-                int result = key;
-
-                //System.out.println("contain:" + result);
-                return result;
-            }
-
-            map.put(state, TURN_IMPOSSIBLE);
-
-            if (hp <= 0) {
-                return TURN_IMPOSSIBLE;
-            }
-
-            if (knightHP - att <=0) {
-                map.put(state, 1);
-                return 1;
-            }
-
-
-            int minimum = TURN_IMPOSSIBLE;
-            int visited;
-
-            int debuffed = knightATT - mDebuff;
-            if (debuffed < 0) {
-                debuffed = 0;
-            }
-
-            if (hp - debuffed <= 0) {
-                //cure
-                visited = simulate(mHPMax - knightATT, att, knightHP, knightATT, commands + 'C') + 1;
-                if (visited < minimum) {
-                    minimum = visited;
-                }
-            } else {
-
-                // debuff
-                if (mDebuff > 0 && knightATT > 0 && !commands.contains("B") && !commands.contains("A") ) {
-                    visited = simulate(hp - debuffed, att, knightHP, debuffed, commands + 'D') + 1;
-                    if (visited < minimum) {
-                        minimum = visited;
-                    }
-                }
-
-                //buff
-                if (mBuff > 0 && !commands.contains("A")) {
-                    visited = simulate(hp - knightATT, att+mBuff, knightHP, knightATT, commands + 'B') + 1;
-                    if (visited < minimum) {
-                        minimum = visited;
-                    }
-                }
-
-                //attack
-                visited = simulate(hp - knightATT, att, knightHP - att, knightATT, commands + 'A') + 1;
-                if (visited < minimum) {
-                    minimum = visited;
-                }
-
-
-
-            }
-
-            map.put(state, minimum);
-            return minimum;
-        }
-
-        class State {
-            public int [] states;
-
-            State(int... values) {
-                states = new int[values.length];
-
-                int i=0;
-                for (int val:values){
-                    states[i++] = val;
-                }
-            }
-
-            @Override
-            public boolean equals(Object object) {
-
-                if (object instanceof State) {
-                    State target = (State) object;
-                    int length = states.length;
-
-                    if (target.states.length == length) {
-                        for (int i=0;i<length;i++) {
-                            if (target.states[i] != states[i]) {
-                                return false;
-                            }
-                        }
-
-                        return true;
-                    }
-                }
-
-                return false;
-            }
-
-            @Override
-            public int hashCode() {
-                int hash = 0;
-                for (int i=0;i<states.length;i++) {
-                    hash += states[i];
-                }
-
-                return hash;
-            }
-
-
-        }
     }
 }
