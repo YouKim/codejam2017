5fac56d99fbe3f469a34942cc82c555b06d2156e
You Kim <you.kim72@gmail.com>
2017-05-28 04:42:49 +0900

    Source Cleanup
    Add another solution in comment.
    Keep stack of K-1 pancake(s) for better performance.

--- src/jam2017/round1C/ProblemA.java
+++ src/jam2017/round1C/ProblemA.java
@@ -22,6 +22,20 @@ public class ProblemA extends Round1C {
 
     static class Pancakes {
 
+        class Cake {
+            long r;
+            long h;
+            long side;
+            long top;
+
+            Cake(int R, int H) {
+                r = R;
+                h = H;
+                side = 2 * r * h;
+                top = r * r;
+            }
+        }
+
         final int N;
         final int K;
 
@@ -65,12 +79,10 @@ public class ProblemA extends Round1C {
 
             while (cakes.size() >= K) {
 
-                long area = 0;
-
                 Cake bottom = cakes.remove(0);
                 sides.remove(bottom);
 
-                area += bottom.top + bottom.side;
+                long area = bottom.top + bottom.side;
 
                 for (int i=0;i+1<K;i++) {
                     Cake stack = sides.get(i);
@@ -85,18 +97,56 @@ public class ProblemA extends Round1C {
             return maximumArea * Math.PI;
         }
 
-        class Cake {
-            long r;
-            long h;
-            long side;
-            long top;
+        /* another solution. keep K-1 stack of cakes on top of base pancake.
+        double solve2() {
+            Collections.sort(cakes, new Comparator<Cake>() {
+                @Override
+                public int compare(Cake o1, Cake o2) {
+                    return (o2.r > o1.r)?1:(o2.r < o1.r)?-1:0;
+                }
+            });
 
-            Cake(int R, int H) {
-                r = R;
-                h = H;
-                side = 2 * r * h;
-                top = r * r;
+            Collections.sort(sides, new Comparator<Cake>() {
+                @Override
+                public int compare(Cake o1, Cake o2) {
+                    return (o2.side > o1.side)?1:(o2.side < o1.side)?-1:0;
+                }
+            });
+
+            ArrayList<Cake> stacks = new ArrayList<>();
+            long stackedSide = 0;
+
+            for (int i=0;i+1<K;i++) {
+                Cake stack = sides.remove(0);
+                stacks.add(stack);
+                stackedSide += stack.side;
             }
+
+            long maximumArea = 0;
+
+            while (cakes.size() >= K) {
+
+                Cake bottom = cakes.remove(0);
+                sides.remove(bottom);
+
+                boolean removed = stacks.remove(bottom);
+                if (removed) {
+                    stackedSide -= bottom.side;
+                    Cake newCake = sides.remove(0);
+
+                    stacks.add(newCake);
+                    stackedSide += newCake.side;
+                }
+
+                long area = bottom.top + bottom.side + stackedSide;
+
+                if (maximumArea < area) {
+                    maximumArea = area;
+                }
+            }
+
+            return maximumArea * Math.PI;
         }
+        */
     }
 }
