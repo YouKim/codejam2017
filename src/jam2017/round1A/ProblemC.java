aeff914b211313e7dfdeb1e6542cb56c27799a08
U-dolzzo-PC\dolzzo <dolzzo@mutecsoft.com>
2017-06-05 19:43:11 +0900

    [WIP] Remove Attack/Buff from calculation
    Focusing on Debuff/Cure turns.

--- src/jam2017/round1A/ProblemC.java
+++ src/jam2017/round1A/ProblemC.java
@@ -19,65 +19,241 @@ public class ProblemC extends Round1A {
     }
 
     static class Game {
-        // Targeted to large 10^9
-        static final int ACTION_MAX = 1000000000;
-        static final int TURN_MAX = 4*ACTION_MAX;  //AB combo <= 10^9 + D <=10^9 + C can interleave all action.
+        static final String IMPOSSIBLE = "IMPOSSIBLE";
         static final int TURN_IMPOSSIBLE = Integer.MAX_VALUE;
 
-        final int mHd, mAd, mHk, mAk, mB, mD;
-        int minimumAB = TURN_IMPOSSIBLE;
-        int minimumWin = TURN_IMPOSSIBLE;
+        final int mHd, mAk, mD, mAB, mDmax;
 
         Game(InputReader in) {
+            int Ad, Hk, B;
+
             mHd = in.nextInt();
-            mAd = in.nextInt();
-            mHk = in.nextInt();
+            Ad = in.nextInt();
+            Hk = in.nextInt();
             mAk = in.nextInt();
-            mB = in.nextInt();
+            B = in.nextInt();
             mD = in.nextInt();
+
+            mAB = findMinAB(Ad, Hk, B);
+            mDmax = Ceil(mAk, mD);
+
+            System.out.println("mDmax:"+(mAB+mDmax)+" Hd:"+mHd+" Ad:"+Ad+" Hk:"+Hk+" Ak:"+mAk+" B:" + B + " D:" + mD);
         }
 
-        // Maximum value will be 100 in small or 10^9 in large
-        int findOptimalBuff() {
-            if (mB <= 0) {
-                minimumAB = attackCount(mHk, mAd);
-                System.out.println("Minimal Attack & Buff combo:" + minimumAB);
-                return 0;
+        int findMinAB(int Ad, final int Hk, final int Buff) {
+            if (Buff <= 0) {
+                return Ceil(Hk, Ad);
             } else {
-                int optB = TURN_IMPOSSIBLE;
-                int minAB = ACTION_MAX;
-
-                for (int B=0;B<minAB;B++) {
-                    int A = attackCount(mHk, mAd + mB * B);
+                int minAB = TURN_IMPOSSIBLE;
+                for (int B=0;B<Hk;B++) {
+                    int A = Ceil(Hk, Ad);
                     if (A + B < minAB) {
                         minAB = A + B;
-                        optB = B;
+                    } else {
+                        break;
                     }
+
+                    Ad += Buff;
                 }
-                minimumAB = minAB;
-                System.out.println("Minimal Attack & Buff combo:" + minimumAB);
-                return optB;
+                //System.out.println("B:"+B);
+                return minAB;
             }
         }
 
-        int attackCount(int hp, int attack) {
+        public String solve() {
+            // TODO Auto-generated method stub
+
+            int minT = Integer.MAX_VALUE;
+
+            if (mDmax == 0) {
+                minT = simulateD(0);//sumulateNoDebuff();
+            } else {
+                int D;
+                for (D = 0 ;D <= mDmax;D++) {
+                    int T = simulateD(D);
+
+                    if (T < minT) {
+                        minT = T;
+                    }
+                }
+            }
+
+            if (minT<Integer.MAX_VALUE) {
+                return String.valueOf(minT);
+            } else {
+                return IMPOSSIBLE;
+            }
+        }
+
+        static int Ceil(int A, int B) {
+            if (B<=0) {
+                return 0;
+            }
+
+            return A/B + (A%B>0?1:0);
+        }
+
+        static int Floor(int A, int B) {
+            if (B<=0) {
+                return 0;
+            }
+
+            return A/B - (A%B>0?0:1);
+        }
+
+        int sumulateNoDebuff() {
+            int Cint = Floor(mHd, mAk);
+
+            int C;
+
+            if (Cint == 0) {
+                System.out.println("C == 0");
+                C = 0;
+            } else if (Cint == 1 && mAB > 2) {
+                System.out.println("Cint == 1");
+                return TURN_IMPOSSIBLE;
+            } else {
+                C = Floor(mAB, Cint+1);
+            }
+
+            System.out.println("mAB:" + mAB + " C:" + C + " Cint:" + Cint);
+            int total = mAB + C;
+            return total;
+        }
+
+        int simulateD(int Dcnt) {
+
+            int AB = mAB;
+            int D = 0;
+            int Ak = mAk;
+            int Hd = mHd;
+            //int C = 0;
+            boolean cure = false;
+
+            for (int T = 1; true ; T++) {
+                // Final Blow
+                if (AB <= 1) {
+                    System.out.println("[WIN] T:" + T);
+                    return T;
+                }
+
+
+                int Ak_d = Ak;
+                if (D < Dcnt) {
+                    Ak_d = Ak - mD;
+                    if (Ak_d < 0) {
+                        Ak_d = 0;
+                    }
+                }
+
+                if (Hd - Ak_d <= 0) {
+                    // cure
+                    //C++;
+                    Hd = mHd;
+                    if (cure) {
+                        return TURN_IMPOSSIBLE;
+                    }
+
+                    cure = true;
+
+                    /*
+                    if (D >= Dcnt) {
+                        int Cint = Floor(mHd, Ak);
+
+                        int C;
+
+                        if (Cint == 0) {
+                            C = 0;
+                        } else if (Cint == 1) {
+                            return TURN_IMPOSSIBLE;
+                        } else {
+                            C = Floor(AB, Cint-1);
+                        }
+                        int total = T + AB + C;
+                        return total;
+                    }
+                    */
+
+                } else {
+                    cure = false;
+                    if (D < Dcnt) {
+                        D++;
+                        Ak = Ak_d;
+                    } else {
+                        AB--;
+                    }
+                }
+
+                // Knight's turn.
+                Hd -= Ak;
+                if (Hd <= 0) {
+                    System.out.println("[LOST] T:" + T);
+                    break;
+                }
+            }
+
+            return TURN_IMPOSSIBLE;
+        }
+
+
+        /*
+
+        // Maximum value will be 100 in small or 10^9 in large
+        int findMinAB() {
+            if (mB <= 0) {
+                return Ceil(mHk, mAd);
+            } else {
+                long minAB = TURN_IMPOSSIBLE;
+                int B;
+
+                for (B=0;B<mHk;B++) {
+                    long A = Ceil(mHk, mAd + mB * B);
+                    if (A + B < minAB) {
+                        minAB = A + B;
+                    } else {
+                        break;
+                    }
+                }
+                System.out.println("B:"+B);
+                return minAB;
+            }
+        }
+
+        long attackCount(long hp, long attack) {
             return hp/attack + (hp%attack>0?1:0);
         }
 
-        int findDebuffLimit() {
+        long findDebuffLimit() {
             if (mD <= 0) {
                 return 0;
             } else {
-                int result = mAk/mD + (mAk%mD>0?1:0);
+                long result = mAk/mD + (mAk%mD>0?1:0);
                 return result;
             }
         }
 
-        // Check if 1st turn is cure;
-        boolean isImpossible() {
-            return ((mHk - mAd > 0) && (mHd - (mAk - mD)) < 0);
+        long cureInterval(long hp, long attack) {
+            if (attack <= 0) {
+                return Integer.MAX_VALUE;
+            }
+            return hp/attack - (hp%attack>0?0:1);
         }
 
+        long cureCount(long cureInterval, long total) {
+
+            return total/cureInterval - (total%cureInterval>0?0:1);
+        }
+
+        int cureTurn(int turn, int interval) {
+            return turn/interval - (turn%interval>0?0:1);
+        }
+
+        // Check if 1st turn is cure;
+        boolean isImpossible() {
+            return ((mHk - mAd > 0) && (mHd - (mAk - mD)) <= 0);
+        }
+
+        /*
         int simulate(int BuffLimit, int DebuffLimit) {
 
             int Hd = mHd, Ad = mAd, Hk = mHk, Ak = mAk;
@@ -86,14 +262,13 @@ public class ProblemC extends Round1A {
             StringBuffer commands = new StringBuffer();
             System.out.println("T:"+0+" Hd:"+Hd+" Ad:"+Ad+" Hk:"+Hk+" Ak:"+Ak+" B:"+mB+" D:"+mD+" BL:"+BuffLimit+" DL"+DebuffLimit);
 
-            for (int T = 1; T < minimumWin; T++) {
+            for (int T = 1; T < mTurnMax; T++) {
                 // Final Blow
                 if (Hk - Ad <= 0) {
                     A++;
                     //Hk -= Ad;
                     commands.append('A');
                     System.out.println("[WIN] T:"+T+" A:"+A+" B:"+B+" C:"+C+" D:"+D+" cmds:"+commands);
-                    minimumWin = T;
                     return T;
                 }
 
@@ -145,22 +320,89 @@ public class ProblemC extends Round1A {
             return TURN_IMPOSSIBLE;
         }
 
+
+
+
+        int simulate(int D, int AB) {
+            return 0;
+        }
+
         String solve() {
             int Tmin = TURN_IMPOSSIBLE;
             if (!isImpossible()) {
-                int B = findOptimalBuff();
-                int dLimit = findDebuffLimit();
-                for (int D=0;D<=dLimit;D++) {
-                    int T = simulate(B, D);
-                    Tmin = Math.min(Tmin, T);
+                long AB = findMinAB();
+
+                System.out.println("AB"+AB);
+
+                if (mD > 0) {
+                    long limit = mAk/mD + (mAk%mD>0?1:0);
+
+                    long minCure = Integer.MAX_VALUE;
+                    System.out.println("limit:"+limit);
+                    for (long D=limit;D>=0;) {
+                        long I = cureInterval(mHd, mAk-D*mD);
+
+                        if (I == 0) {
+                            D--;
+                            continue;
+                        }
+
+                        long T = cureCount(I, D+AB);
+                        System.out.println("I:"+I + " D:" + D);
+                        System.out.println("T:"+(T+D+AB));
+
+                        //System.out.println("I:"+I + " D:" + D);
+                        //System.out.println("mHd:"+mHd + " mAk:" + mAk + " mD:" + mD);
+                        if (I<=minCure && I > 1) {
+
+                            minCure = I;
+
+                            long targetAttack = mHd/(I-1) + (mHd%(I-1)>0?1:0);
+
+
+
+                            long newD = (mAk - targetAttack)/mD + (mAk%targetAttack>0?1:0);
+
+                            if (newD < D) {
+                                D = newD;
+                            } else {
+                                break;
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+                    /*
+                    for (int D=0;D<=limit;D++) {
+
+                        int I = cureInterval(mHd, mAk-D*mD);
+                        System.out.println("I:"+I + " D:" + D);
+                        if (I>minCure) {
+
+                            minCure = I;
+                            int T = simulate(mOptB, D);
+
+                            if (T < Tmin) {
+                                Tmin = T;
+                            }
+
+
+                            D = (mAk - mHd / (I+1)) * mD;
+                        }
+                    }
+                    */
+        /*
+                } else {
+                    Tmin = simulate(mOptB, 0);
                 }
             }
 
             if (Tmin<TURN_IMPOSSIBLE) {
                 return String.valueOf(Tmin);
             } else {
-                return "IMPOSSIBLE";
+                return IMPOSSIBLE;
             }
         }
+        */
     }
 }
